b9523fbf5e69b638f8ae26b0d0d1b247d404a1b0
Dima2009-crupto <dimaparoldima1234@gmail.com>
2025-02-15 09:01:57 +0100

    1 1

--- src/data/parse_data.py
+++ src/data/parse_data.py
@@ -1,16 +1,16 @@
-import logging 
+import logging
 import sys
 from uuid import uuid4
 
 from requests_html import HTMLSession
 
-from src.database.models import Product, db
+from src.database.models import Product
+from src.database.base import db
 
 
-URL = "https://rozetka.com.ua/ua/mobile-phones/c80003/"
+URL = "https://rozetka.com.ua/mobile-phones/c80003/"
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
 
 def get_products(url: str = URL):
     session = HTMLSession()
@@ -18,7 +18,7 @@ def get_products(url: str = URL):
 
     products = response.html.xpath('//a[@class="ng-star-inserted" and span[@class="goods-tile__title ng-star-inserted"]]/@href')
     for i, product in enumerate(products, start=1):
-        LOG.info(f"Товар № {i} відправлено на опрацювання. url={product}")
+        LOG.info(f"Товар № {i} відправлено на опрацювання")
         save_product(product)
 
 
@@ -30,15 +30,15 @@ def save_product(url):
     img_url = response.html.xpath('//div[@class="main-slider__wrap ng-star-inserted"]//img/@src')[0]
     description = response.html.xpath('//rz-if-in-view[@class="lazy-load-container ng-star-inserted"]//text()')
     description = "".join(description)
-    price = response.html.xpath('//p[contains(@class, "product-price__big")]/text()')[0].replace(u"\xa0", "")
+    price = response.html.xpath('//p[contains(@class,"product-price__big")]/text()')[0].replace(u"\xa0", "")
     product = Product(
         id=uuid4().hex,
         name=name,
         img_url=img_url,
         description=description,
         price=price
-    )
+)
 
     db.session.add(product)
     db.session.commit()
-    LOG.info(f"Товар '{name}' успішно збережено")
+    LOG.info(f"Товар '{name}' успішно збережено!")
--- src/database/db_actions.py
+++ src/database/db_actions.py
@@ -1,43 +1,47 @@
 from uuid import uuid4
 from typing import List
 
-from src.database.models import db, Product, Review
+from src.database.models import Product, Review, User
+from src.database.base import db
 
 
-def get_products():
+def get_products() -> List[Product]:
+    # return db.get_or_404(db.session.query(Product))
     return db.session.query(Product).all()
 
 
-def get_product(prod_id: str):
-    return db.session.query(Product).where(Product.id == prod_id).first()
+def get_product(prod_id: str) -> Product:
+    return db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
+    # return db.session.query(Product).where(Product.id == prod_id).first()
 
 
-def add_products(
-        name: str,
-        descrition:str,
-        img_url: str,
-        price: float
-)-> str:
+def add_product(
+    name: str,
+    description: str,
+    img_url: str,
+    price: float
+) -> str:
     product = Product(
         id=uuid4().hex,
         name=name,
-        descrition=descrition,
+        description=description,
         img_url=img_url,
         price=price
     )
     db.session.add(product)
     db.session.commit()
+    db.session.refresh(product)
     return product.id
 
 
 def edit_product(
-    prod_id: str,
-    name: str,
-    description: str,
-    img_url: str,
-    price: float
-)->str:
-    product = db.session.query(Product).where(Product.id == prod_id).first()
+        prod_id: str,
+        name: str,
+        description: str,
+        img_url: str,
+        price: float
+) -> str:
+    product = db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
     product.name = name
     product.description = description
     product.img_url = img_url
@@ -47,11 +51,12 @@ def edit_product(
 
 
 def del_product(prod_id: str) -> str:
-    product = db.session.query(Product).where(Product.id == prod_id).first()
+    product = db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
     db.session.delete(product)
     db.session.commit()
     return "Successful"
 
+# ВІДГУКИ
 
 def get_reviews() -> List[Review]:
     return db.session.query(Review).all()
@@ -97,3 +102,30 @@ def del_review(rev_id: str) -> str:
     db.session.delete(review)
     db.session.commit()
     return "Successful"
+
+
+def add_user(
+    email: str,
+    password: str,
+    first_name: str|None = None,
+    last_name: str|None = None
+) -> str:
+    user = User(
+        id=uuid4().hex,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password
+    )
+    db.session.add(user)
+    db.session.commit()
+    return "Successful"
+
+
+def get_user(user_id: str):
+    return db.one_or_404(db.session.query(User).where(User.id==user_id))
+
+
+def get_tokens(email: str, password: str) -> dict|None:
+    user = db.one_or_404(db.session.query(User).where(User.email==email))
+    return user.get_tokens(password)
